3bcb14656fc4e1b90741b23263ef575679770a6f
Nikki <62213020+nikkipanda-dev@users.noreply.github.com>
2022-05-08 20:49:16 +0800

    test api login

--- src/components/widgets/Login/index.js
+++ src/components/widgets/Login/index.js
@@ -85,10 +85,10 @@ export const Form = ({
                 if (Cookies.get('auth_user') && Cookies.get('auth_user_token')) {
                     console.log('valid')
 
-                    // setTimeout(() => {
-                    //     handleLogIn();
-                    //     navigate('/dashboard');
-                    // }, 1000);
+                    setTimeout(() => {
+                        handleLogIn();
+                        navigate('/dashboard');
+                    }, 1000);
                 }
             } else {
                 console.log('no')
