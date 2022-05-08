0f6ebe9b13fc6ae20b9d938787a59ee5fbed6bad
Nikki <62213020+nikkipanda-dev@users.noreply.github.com>
2022-05-08 20:43:25 +0800

    test api login

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    timeout: 1000,
+    // timeout: 1000,
     headers: { 
         'X-Requested-With': 'XMLHttpRequest',
         'Accept': 'application/json',
